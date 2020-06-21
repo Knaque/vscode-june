00c54fb1bbc9ca01d9468eefbb50c2d961f6d01b
Reilly Moore <williammoore6105@gmail.com>
2020-06-21 00:46:39 -0500

    stuff

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# vscode-june
-A combination personal/pride month theme for VSCode.
+# June theme
+
+A combination personal/pride month theme for VSCode. Definitely not the best
+theme, but I made it for myself.
+
+[Repository](https://github.com/Knaque/vscode-june)
+
+[VSCode Marketplace](https://marketplace.visualstudio.com/items?itemName=knaque.june-theme)
+
+Created with [Tyriar/vscode-theme-generator](https://github.com/Tyriar/vscode-theme-generator)
---
+++ index.ts
@@ -0,0 +1,57 @@
+import * as path from 'path';
+import { generateTheme, IColorSet } from 'vscode-theme-generator';
+
+const colorSet: IColorSet = {
+  type: 'dark',
+  base: {
+    background: '#0c090d',
+    foreground: '#ffffc7',
+    color1: '#53b3cb',
+    color2: '#e01a4f',
+    color3: '#218380',
+    color4: '#f9c22e'
+  },
+  syntax: {
+    boolean: '#e01a4f',
+    function: '#f9c22e',
+    functionCall: '#f9c22e',
+    identifier: '#f15946',
+    keyword: '#e01a4f',
+    number: '#e01a4f',
+    storage: '#f15946',
+    string: '#53b3cb',
+    stringEscape: '#e01a4f',
+    comment: '#48453A',
+    cssTag: '#ffffc7',
+    markdownQuote: '#f9c22e'
+  },
+  ui: {
+    cursor: '#ffffc7',
+    invisibles: '#48453A', // might want to tweak
+    guide: '#292623',
+    currentFindMatchHighlight: '#400D1D',
+    selectionHighlight: '#292623',
+    selection: '#292623',
+    activeLinkForeground: '#4F7FFF'
+  },
+  terminal: {
+    black: '#292623',
+    red: '#e01a4f',
+    green: '#218380',
+    yellow: '#f9c22e',
+    blue: '#4F7FFF',
+    magenta: '#C210E0',
+    cyan: '#49CC9E',
+    white: '#ffffc7',
+    brightBlack: '#292623',
+    brightRed: '#e01a4f',
+    brightGreen: '#218380',
+    brightYellow: '#f9c22e',
+    brightBlue: '#4F7FFF',
+    brightMagenta: '#C210E0',
+    brightCyan: '#49CC9E',
+    brightWhite: '#ffffc7'
+  }
+};
+
+generateTheme('June', colorSet, path.join(__dirname, 'theme.json'));
