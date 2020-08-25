3a956ffeb2084c51dbfce7d7b67ab870565b06f7
Reilly Moore <williammoore6105@gmail.com>
2020-08-25 08:58:16 -0500

    fresh colors

--- index.ts
+++ index.ts
@@ -1,56 +1,70 @@
 import * as path from 'path';
 import { generateTheme, IColorSet } from 'vscode-theme-generator';
 
+const white = "#fcfcfc"
+const red = "#e01a4f"
+const orange = "#f15f45"
+const yellow = "#fad537"
+const green = "#98cb32"
+const blue = "#53b5cb"
+const black = "#141017"
+const bw95 = "#6A676C"
+const bw31 = "#302C32"
+const by63 = "#4C401E"
+const magenta = "#F113A5"
+const cyan = "#399B98"
+const dblue = "#4E76D9"
+
 const colorSet: IColorSet = {
   type: 'dark',
   base: {
-    background: '#141017', // black
-    foreground: '#FCFCFC', // white
-    color1: '#53B5CB', // blue
-    color2: '#E01A4F', // red
-    color3: '#218380', // green
-    color4: '#F9CC2E' // yellow
+    background: black,
+    foreground: white,
+    color1: blue,
+    color2: red,
+    color3: green,
+    color4: yellow
   },
   syntax: {
-    boolean: '#E01A4F', // red
-    function: '#F9CC2E', // yellow
-    functionCall: '#F9CC2E', // yellow
-    identifier: '#F15946', // orange
-    keyword: '#E01A4F', // red
-    number: '#E01A4F', // red
-    storage: '#F15946', // orange
-    string: '#53B5CB', // blue
-    stringEscape: '#E01A4F', // red
-    comment: '#6A676C', // 95 black-white
-    cssTag: '#FCFCFC', // white
-    markdownQuote: '#F9CC2E' // yellow
+    boolean: red,
+    function: yellow,
+    functionCall: yellow,
+    identifier: orange,
+    keyword: red,
+    number: red,
+    storage: orange,
+    string: blue,
+    stringEscape: red,
+    comment: bw95,
+    cssTag: white,
+    markdownQuote: yellow
   },
   ui: {
-    cursor: '#FCFCFC', // white
-    invisibles: '#6A676C', // 95 black-white
-    guide: '#302C32', // 31 black-white
-    currentFindMatchHighlight: '#4C3E1C', // 63 black-yellow
-    selectionHighlight: '#302C32', // 31 black-white
-    selection: '#302C32', // 31 black-white
-    activeLinkForeground: '#4F82FF' // custom blue
+    cursor: white,
+    invisibles: bw95,
+    guide: bw31,
+    currentFindMatchHighlight: by63,
+    selectionHighlight: bw31,
+    selection: bw31,
+    activeLinkForeground: dblue
   },
   terminal: {
-    black: '#6A676C', // 95 black-white
-    red: '#E01A4F',
-    green: '#218380',
-    yellow: '#F9CC2E',
-    blue: '#4F82FF', // custom blue
-    magenta: '#F914FA', // custom magenta
-    cyan: '#49CC9E', // custom cyan
-    white: '#FCFCFC',
-    brightBlack: '#6A676C', // 95 black-white
-    brightRed: '#E01A4F',
-    brightGreen: '#218380',
-    brightYellow: '#F9CC2E',
-    brightBlue: '#4F82FF', // custom blue
-    brightMagenta: '#F914FA', // custom magenta
-    brightCyan: '#49CC9E', // custom cyan
-    brightWhite: '#FCFCFC'
+    black: bw95,
+    red: red,
+    green: green,
+    yellow: yellow,
+    blue: blue,
+    magenta: magenta,
+    cyan: cyan,
+    white: white,
+    brightBlack: bw95,
+    brightRed: red,
+    brightGreen: green,
+    brightYellow: yellow,
+    brightBlue: blue,
+    brightMagenta: magenta,
+    brightCyan: cyan,
+    brightWhite: white
   }
 };
 
